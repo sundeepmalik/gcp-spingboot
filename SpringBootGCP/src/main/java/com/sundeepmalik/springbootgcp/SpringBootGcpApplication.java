209b89f0fb87b8efd72947fff49c87abdc7198d6
sundeepmalik <sundeepmalik007@gmail.com>
2018-03-13 11:20:56 -0400

    commiting project

---
+++ SpringBootGCP/src/main/java/com/sundeepmalik/springbootgcp/SpringBootGcpApplication.java
@@ -0,0 +1,12 @@
+package com.sundeepmalik.springbootgcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGcpApplication.class, args);
+	}
+}
